99e7dd22bc47ab75ec4a97f24453fe1574fa94b5
tamaramck88 <tmcknight88@yahoo.com>
2026-02-01 16:35:02 +0200

    final

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,15 +1,13 @@
-// Footer Dates
-const yearSpan = document.querySelector("#year");
-const lastModifiedSpan = document.querySelector("#lastModified");
+const year = document.querySelector("#year");
+const lastModified = document.querySelector("#lastModified");
 
-yearSpan.textContent = new Date().getFullYear();
-lastModifiedSpan.textContent = document.lastModified;
+year.textContent = new Date().getFullYear();
+lastModified.textContent = document.lastModified;
 
-// Hamburger Menu
 const menuButton = document.querySelector("#menu-button");
-const navMenu = document.querySelector("#nav-menu");
+const nav = document.querySelector("#nav-menu");
 
 menuButton.addEventListener("click", () => {
-  navMenu.classList.toggle("open");
-  menuButton.textContent = navMenu.classList.contains("open") ? "✖" : "☰";
+  nav.classList.toggle("open");
+  menuButton.textContent = nav.classList.contains("open") ? "✖" : "☰";
 });
