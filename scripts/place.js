d4cbe76682e9fdb8d426d47d1e67af748ee031e7
tamaramck88 <tmcknight88@yahoo.com>
2026-02-04 23:04:47 +0200

    changes

--- scripts/place.js
+++ scripts/place.js
@@ -1,28 +1,19 @@
-// Footer dates
 const yearSpan = document.querySelector("#year");
 const lastModified = document.querySelector("#lastModified");
 
 yearSpan.textContent = new Date().getFullYear();
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
 
-// Weather values (static, as required)
 const temperature = Number(document.querySelector("#temp").textContent);
 const windSpeed = Number(document.querySelector("#wind").textContent);
 const chillSpan = document.querySelector("#chill");
 
-// Wind chill calculation (Metric: °C, km/h)
 function calculateWindChill(temp, speed) {
-  return (
-    13.12 +
-    0.6215 * temp -
-    11.37 * Math.pow(speed, 0.16) +
-    0.3965 * temp * Math.pow(speed, 0.16)
-  ).toFixed(1);
+    return (13.12 + 0.6215 * temp - 11.37 * Math.pow(speed, 0.16) + 0.3965 * temp * Math.pow(speed, 0.16)).toFixed(1);
 }
 
-// Apply conditions before calculating wind chill
 if (temperature <= 10 && windSpeed > 4.8) {
-  chillSpan.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
+    chillSpan.textContent = `${calculateWindChill(temperature, windSpeed)} °C`;
 } else {
-  chillSpan.textContent = "N/A";
+    chillSpan.textContent = "N/A";
 }
